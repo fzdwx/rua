cfc9374cfae3c9e00a0b8a8d1c1dbcde3a95806e
like <likelovec@gmail.com>
2025-12-10 13:26:23 +0800

    chore: enhance window toggle functionality and add installation script

--- justfile
+++ justfile
@@ -6,3 +6,9 @@ build:
 
 bump version:
     sh bump-version.sh {{version}}
+
+install:
+    jq '.bundle.active = false' src-tauri/tauri.conf.json > src-tauri/tauri.conf.json.tmp && mv src-tauri/tauri.conf.json.tmp src-tauri/tauri.conf.json
+    bun run build
+    sudo cp src-tauri/target/release/rua /usr/bin/
+    jq '.bundle.active = true' src-tauri/tauri.conf.json > src-tauri/tauri.conf.json.tmp && mv src-tauri/tauri.conf.json.tmp src-tauri/tauri.conf.json
--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,32 @@ import {register} from '@tauri-apps/plugin-global-shortcut';
 import {getCurrentWindow} from "@tauri-apps/api/window";
 import {ActionUsageProvider} from "@/hooks/useActionUsage";
 import {WeatherConfigProvider} from "@/hooks/useWeatherConfig";
+import {useKeyPress} from "ahooks";
 
-register('Alt+Space', async (e) => {
-    if (e.state == 'Released') {
-        let currentWindow = getCurrentWindow();
-        let b = await currentWindow.isVisible();
-        if (b) {
-            await currentWindow.hide()
-        } else {
-            await currentWindow.center()
-            await currentWindow.show()
-            await currentWindow.setFocus()
-        }
+register('Alt+Space', async (_e) => {
+    let currentWindow = getCurrentWindow();
+    let b = await currentWindow.isVisible();
+    if (b) {
+        await currentWindow.hide()
+    } else {
+        await currentWindow.center()
+        await currentWindow.show()
+        await currentWindow.setFocus()
     }
 }).catch(e => {
     console.log(e)
 });
 
 function AppContent() {
+    // 在 wayland 下，如果聚焦了当前窗口，再次使用快捷键是无法触发的
+    useKeyPress("alt.space", async (_e) => {
+        let currentWindow = getCurrentWindow();
+        const b = await currentWindow.isVisible()
+        if (b) {
+            await currentWindow.hide()
+        }
+    })
+
     return (
         <>
             <Home/>
