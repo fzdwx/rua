a3a2f90e7ef52f2293cfba2cb5c57ddf639ecf99
fzdwx <likelovec@gmail.com>
2025-12-14 16:28:35 +0800

    feat: section

--- apps/rua/src/home/DefaultView.tsx
+++ apps/rua/src/home/DefaultView.tsx
@@ -66,7 +66,7 @@ export function DefaultView({
                         onRender={({item, active}) => {
                             if (typeof item === "string") {
                                 return (
-                                    <div className="px-4 py-2 text-xs font-semibold text-gray-11 uppercase tracking-wide">
+                                    <div className="px-4 py-2 text-xs font-semibold text-gray-11 tracking-wide">
                                         {item}
                                     </div>
                                 );
